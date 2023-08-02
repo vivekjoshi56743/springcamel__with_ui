cf822420a84b6d9758935dd370ef1f6f9544f7fa
vivek joshi <81894841+vivekjoshi56743@users.noreply.github.com>
2023-08-02 21:25:31 +0530

    done final changes

--- camel-project/src/main/java/com/vivek/camelproject/router/CamelRoute.java
+++ camel-project/src/main/java/com/vivek/camelproject/router/CamelRoute.java
@@ -11,50 +11,35 @@ public class CamelRoute extends RouteBuilder {
 
 	private int pageNo;
 	private List<Integer> ids;
-	 private List<String> filteredData=new ArrayList<>();
+	private List<String> filteredData = new ArrayList<>();
 
 	@Override
 	public void configure() throws Exception {
 		// TODO Auto-generated method stub
-		
-		
+
 		from("direct:fetchUserData")
-		
-        .process(exchange -> {
-        	 
-        	pageNo = exchange.getIn().getHeader("pageNo", Integer.class);
-        	 
-        	 setIds(exchange.getIn().getHeader("ids", List.class));
-        	filteredData.clear(); 
-            
-        })
-        .setHeader("CamelHttpMethod", constant("GET")) // Set the HTTP method to GET
-        .setHeader("CamelHttpQuery", simple("page=${header.pageNo}")) // Set the query parameter
-        .toD("https://reqres.in/api/users")
-        .split().jsonpath("$.data")
-        .process(exchange -> {
-        	String data=exchange.getIn().getBody(String.class);
-        	String idString = data.substring(data.indexOf('=') + 1, data.indexOf(',')).trim();
-        	int id = Integer.parseInt(idString);
-        	if(getIds().contains(-1)) {
-        		addfiltereddata(data);
-        	}
-        	else if(getIds().contains(id)) {
-        		addfiltereddata(data);
-        		//System.out.println(data);
-        	}
-        	//System.out.println(message);
-        	//System.out.println(getIds());
-        	
-        	
-        } );
-        
-		
-		
 
-        
+				.process(exchange -> {
 
-		
+					pageNo = exchange.getIn().getHeader("pageNo", Integer.class);
+
+					setIds(exchange.getIn().getHeader("ids", List.class));
+					filteredData.clear();
+
+				}).setHeader("CamelHttpMethod", constant("GET"))
+				.setHeader("CamelHttpQuery", simple("page=${header.pageNo}")).toD("https://reqres.in/api/users").split()
+				.jsonpath("$.data").process(exchange -> {
+					String data = exchange.getIn().getBody(String.class);
+					String idString = data.substring(data.indexOf('=') + 1, data.indexOf(',')).trim();
+					int id = Integer.parseInt(idString);
+					if (getIds().contains(-1)) {
+						addfiltereddata(data);
+					} else if (getIds().contains(id)) {
+						addfiltereddata(data);
+					}
+
+				});
+
 	}
 
 	public List<Integer> getIds() {
@@ -72,17 +57,9 @@ public class CamelRoute extends RouteBuilder {
 	public void setFilteredData(List<String> filteredData) {
 		this.filteredData = filteredData;
 	}
-	
-	
-	private void addfiltereddata(String data ) {
+
+	private void addfiltereddata(String data) {
 		this.filteredData.add(data);
 	}
-	
 
 }
-
-
-
-
-
-
